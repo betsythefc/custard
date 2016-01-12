b27565c9a656a644b4a83d6ef8b0b706bd052ff6
Bryce McNab <bmcnab80+gitub@gmail.com>
2016-01-11 21:11:03 -0800

    It now kind of works!

---
+++ Documents/Personal/FOSS/custard/readme.md
@@ -0,0 +1,7 @@
+Project:	Custard
+Author:		Bryce McNab
+Version:	0.01
+
+Dependencies
+============
+Apache Web Server
---
+++ Documents/Personal/FOSS/custard/test.php
@@ -0,0 +1,18 @@
+<?php
+//Database Credentials
+$host = 'localhost';
+$database = 'custard';
+$username = 'custard_admin';
+$password = 'apache';
+
+try {
+	$DBH = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+}
+
+catch(PDOException $e) {
+	echo $e->getMessage();
+}
+
+echo "it works";
+?>
+<b>Look</b>
