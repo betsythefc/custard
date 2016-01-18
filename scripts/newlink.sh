547b83c4eccf69719b0856c198070e1e28a5cbd9
Bryce McNab <bmcnab80+gitub@gmail.com>
2016-01-17 16:45:14 -0800

    Added a folder create in newlink.sh

--- scripts/newlink.sh
+++ scripts/newlink.sh
@@ -2,6 +2,10 @@
 
 RandomNumber=$RANDOM
 
+#Set up folder
+
+mkdir ../links
+
 #Embedded HTML
 
 echo -e "<a href=\"${RandomNumber}.php?review=2\"><img src=\"http://images.clipartpanda.com/smiley-face-png-96527038_o.png\" width=150px height=150px></a>\n<a href=\"${RandomNumber}.php?review=1\"><img src=\"http://www.clker.com/cliparts/I/X/g/L/q/2/yellow-neutral-face-hi.png\" width=150px height=150px></a>\n<a href=\"${RandomNumber}.php?review=0\"><img src=\"http://images.clipartpanda.com/red-smiley-face-png-MiLkkBAia.png\" width=150px height=150px></a>" >> ../links/${RandomNumber}.html
