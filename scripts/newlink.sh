b300d8a2ef88222c068a82eb332ccb6e97de9386
Bryce McNab <bmcnab80+gitub@gmail.com>
2016-02-01 22:09:00 -0800

    New problem!

--- scripts/newlink.sh
+++ scripts/newlink.sh
@@ -10,4 +10,4 @@ mkdir ../links
 
 echo -e "<a href=\"review.php?review=2&ticket=${RandomNumber}\"><img src=\"http://images.clipartpanda.com/smiley-face-png-96527038_o.png\" width=150px height=150px></a>\n<a href=\"review.php?review=1&ticket=${RandomNumber}\"><img src=\"http://www.clker.com/cliparts/I/X/g/L/q/2/yellow-neutral-face-hi.png\" width=150px height=150px></a>\n<a href=\"review.php?review=0&ticket=${RandomNumber}\"><img src=\"http://images.clipartpanda.com/red-smiley-face-png-MiLkkBAia.png\" width=150px height=150px></a>" >> ../links/${RandomNumber}.html
 
-mysql -u custard -papache -D custard -e "INSERT INTO links VALUES (review);"
+mysql -u custard_admin -papache -D custard -e "INSERT INTO links VALUES (${RandomNumber});"
