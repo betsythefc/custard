90e9bf36027f71c739ffde40abd26ef463838880
Bryce McNab <bmcnab80+gitub@gmail.com>
2016-02-01 21:58:09 -0800

    newlink.sh now only creates a single file and utilizes another generic php page.

--- scripts/newlink.sh
+++ scripts/newlink.sh
@@ -8,6 +8,6 @@ mkdir ../links
 
 #Embedded HTML
 
-echo -e "<a href=\"review.php?review=2\"><img src=\"http://images.clipartpanda.com/smiley-face-png-96527038_o.png\" width=150px height=150px></a>\n<a href=\"review.php?review=1\"><img src=\"http://www.clker.com/cliparts/I/X/g/L/q/2/yellow-neutral-face-hi.png\" width=150px height=150px></a>\n<a href=\"review.php?review=0\"><img src=\"http://images.clipartpanda.com/red-smiley-face-png-MiLkkBAia.png\" width=150px height=150px></a>" >> ../links/${RandomNumber}.html
+echo -e "<a href=\"review.php?review=2&ticket=${RandomNumber}\"><img src=\"http://images.clipartpanda.com/smiley-face-png-96527038_o.png\" width=150px height=150px></a>\n<a href=\"review.php?review=1&ticket=${RandomNumber}\"><img src=\"http://www.clker.com/cliparts/I/X/g/L/q/2/yellow-neutral-face-hi.png\" width=150px height=150px></a>\n<a href=\"review.php?review=0&ticket=${RandomNumber}\"><img src=\"http://images.clipartpanda.com/red-smiley-face-png-MiLkkBAia.png\" width=150px height=150px></a>" >> ../links/${RandomNumber}.html
 
-mysql -u root -p@P@ch312e098 -D custard -e "INSERT INTO links VALUES (review);"
+mysql -u custard -papache -D custard -e "INSERT INTO links VALUES (review);"
