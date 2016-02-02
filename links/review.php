ecfbcb38264e33c0f8e77a97588f4b91161db580
Bryce McNab <bmcnab80+gitub@gmail.com>
2016-02-02 11:19:31 -0800

    Fixed the problem with server 500 error

--- links/review.php
+++ links/review.php
@@ -25,7 +25,7 @@
 				$password = 'apache';
 				$ReviewString = $_GET['review'];
 				$Review = (int)$ReviewString;
-				$Ticket = $_GET['ticket']
+				$Ticket = $_GET['ticket'];
 
 				try {
 					$DBH = new PDO("mysql:host=$host;dbname=$database", $username, $password);
@@ -35,15 +35,16 @@
 					echo $e->getMessage();
 				}
 
-				$sql = $DBH->prepare('SELECT link FROM links WHERE link = $Ticket');
+				$sql = $DBH->prepare("SELECT link FROM links WHERE link = $Ticket");
 				$sql->execute();
 				$result = $sql->fetch();
 
 				if (strpos($result,$Ticket) !== false) {
 					if ($Review == 0 || $Review == 1 || $Review == 2) {
-						$sql = $DBH->prepare("INSERT INTO csat VALUES ($Review,$DateString)");
+						$sql = $DBH->prepare("INSERT INTO csat VALUES ($Review,20160201)");
 						$sql->execute();
-						$sql = $DBH->prepare('DELETE FROM links WHERE link = $Ticket');
+						$sql = $DBH->prepare("DELETE FROM links WHERE link = $Ticket");
+						$sql->execute();
 						echo "Your review has been submitted.";
 					} else {
 						 echo "Whoops, there was a problem with your review. Try resubmitting it.";
